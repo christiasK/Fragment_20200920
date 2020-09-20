4403b5f94cc5d3aa95ef26cdf4b3f5a4eee60eb6
christias <christias@naver.com>
2020-09-20 16:08:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fragment_20200920/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.example.fragment_20200920
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    abstract fun setEvents()
+    abstract fun setValues()
+}

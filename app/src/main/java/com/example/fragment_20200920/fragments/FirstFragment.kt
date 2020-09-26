9b4f29fbbfe62d0b517326ec06e4c2b98d8b5550
christias <christias@naver.com>
2020-09-26 10:11:54 +0900

    액티비티의 xml에 fragment 입력

--- app/src/main/java/com/example/fragment_20200920/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragment_20200920/fragments/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fragment_20200920.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -15,5 +16,13 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        changeTextBtn.setOnClickListener {
+            firstText.text = "텍스트 변경됨"
+        }
     }
 }
